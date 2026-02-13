19d6c7b42a11d865a29014f0c65cb64cbc38e962
Abel <9299622+abelhii@users.noreply.github.com>
2026-02-13 14:20:45 +0000

    added FrontPage

---
+++ src/pages/FrontPage.tsx
@@ -0,0 +1,9 @@
+export function FrontPage() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold text-gray-800">
+        Hacker News
+      </h1>
+    </div>
+  );
+}
